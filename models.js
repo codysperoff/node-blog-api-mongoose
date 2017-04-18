78e302f3012b530a48720e7f0a37e4b2869fedec
Cody Speroff <cnsperoff@gmail.com>
2017-04-17 19:02:39 -0500

    updated server.js to include CRUD ops

--- models.js
+++ models.js
@@ -1,70 +1,102 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
-
-function StorageException(message) {
-    this.message = message;
-    this.name = "StorageException";
-}
-
-const BlogPosts = {
-    create: function(title, content, author, publishDate) {
-        const post = {
-            id: uuid.v4(),
-            title: title,
-            content: content,
-            author: author,
-            publishDate: publishDate || Date.now()
-        };
-        this.posts.push(post);
-        return post;
+// this is our schema to represent a blog post
+const blogPostSchema = mongoose.Schema({
+    // the `name` property is String type and required
+    title: {
+        type: String,
+        required: true
     },
-    get: function(id=null) {
-        // if id passed in, retrieve single post,
-        // otherwise send all posts.
-        if (id !== null) {
-            return this.posts.find(post => post.id === id);
-        }
-        // return posts sorted (descending) by
-        // publish date
-        return this.posts.sort(function(a, b) {
-            return b.publishDate - a.publishDate
-        });
+    content: {
+        type: String,
+        required: true
     },
-    delete: function(id) {
-        const postIndex = this.posts.findIndex(
-            post => post.id === id);
-        if (postIndex > -1) {
-            this.posts.splice(postIndex, 1);
-        }
+    author: {
+        type: String,
+        required: true
     },
-    update: function(updatedPost) {
-        const {id} = updatedPost;
-        const postIndex = this.posts.findIndex(
-            post => post.id === updatedPost.id);
-        if (postIndex === -1) {
-            throw StorageException(
-                `Can't update item \`${id}\` because doesn't exist.`)
-        }
-        this.posts[postIndex] = Object.assign(
-            this.posts[postIndex], updatedPost);
-        return this.posts[postIndex];
+    // the `address` property is an object
+    publishDate: {
+        type: String,
     }
-};
+});
 
-function createBlogPostsModel() {
-    const storage = Object.create(BlogPosts);
-    storage.posts = [];
-    return storage;
+//function StorageException(message) {
+//    this.message = message;
+//    this.name = "StorageException";
+//}
+//
+//const BlogPosts = {
+//    create: function (title, content, author, publishDate) {
+//        const post = {
+//            id: uuid.v4(),
+//            title: title,
+//            content: content,
+//            author: author,
+//            publishDate: publishDate || Date.now()
+//        };
+//        this.posts.push(post);
+//        return post;
+//    },
+//    get: function (id = null) {
+//        // if id passed in, retrieve single post,
+//        // otherwise send all posts.
+//        if (id !== null) {
+//            return this.posts.find(post => post.id === id);
+//        }
+//        // return posts sorted (descending) by
+//        // publish date
+//        return this.posts.sort(function (a, b) {
+//            return b.publishDate - a.publishDate
+//        });
+//    },
+//    delete: function (id) {
+//        const postIndex = this.posts.findIndex(
+//            post => post.id === id);
+//        if (postIndex > -1) {
+//            this.posts.splice(postIndex, 1);
+//        }
+//    },
+//    update: function (updatedPost) {
+//        const {
+//            id
+//        } = updatedPost;
+//        const postIndex = this.posts.findIndex(
+//            post => post.id === updatedPost.id);
+//        if (postIndex === -1) {
+//            throw StorageException(
+//                `Can't update item \`${id}\` because doesn't exist.`)
+//        }
+//        this.posts[postIndex] = Object.assign(
+//            this.posts[postIndex], updatedPost);
+//        return this.posts[postIndex];
+//    }
+//};
+//
+//function createBlogPostsModel() {
+//    const storage = Object.create(BlogPosts);
+//    storage.posts = [];
+//    return storage;
+//}
+
+//gives each instance of our BlogPost model an apiRepr method.
+//We will use this as the standard way to represent blog posts in our API
+//Instead of passing along the document itself, we'll pass along this
+//composite data.
+blogPostSchema.methods.apiRepr = function () {
+
+    return {
+        id: this._id,
+        title: this.title,
+        content: this.content,
+        author: this.author,
+        publishDate: this.publishDate
+    };
 }
 
+//creates a new mongoose model 'BlogPost' that uses the blogPostSchema.
+const BlogPosts = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = {
+    BlogPosts
+};
--- server.js
+++ server.js
@@ -31,8 +31,138 @@ app.get('/', (req, res) => {
 // '/blog-posts/:id', we'll route them to the express
 // router instances we've imported. Remember,
 // these router instances act as modular, mini-express apps.
+app.use(bodyParser.json());
 app.use('/blog-posts', newBlogPostsRouter);
 app.use('/blog-posts', deleteBlogPostsRouter);
+
+app.get('/blog-posts', (req, res) => {
+    //allows users to limit GET requests by title, content, author or date published.
+    const filters = {};
+    const queryableFields = ['title', 'content', 'author', 'publishDate'];
+    queryableFields.forEach(field => {
+        if (req.query[field]) {
+            filters[field] = req.query[field];
+        }
+    });
+
+    //how would I create a query operator that showed posts with certain words in the content or title?
+    BlogPosts
+        .find(filters)
+        // we're limiting so that no more than ten blogposts come up at once
+        .limit(10)
+        // `exec` returns a promise
+        .exec()
+        // success callback: for each blog post we got back, we'll
+        // call the `.apiRepr` instance method we've created in
+        // models.js in order to only expose the data we want the API return.
+        .then(BlogPosts => res.json(
+            BlogPosts.map(blogpost => blogpost.apiRepr());
+        ))
+        .catch(
+            err => {
+                console.error(err);
+                res.status(500).json({
+                    message: 'Internal server error'
+                });
+            });
+});
+
+// can also request by ID
+app.get('/blog-posts/:id', (req, res) => {
+    BlogPosts
+        .findById(req.params.id)
+        .exec()
+        .then(blogpost => res.json(blogpost.apiRepr())) //********Q: Will this work here just saying blogposts?********
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        });
+});
+
+//creates a new blog post
+app.post('/blog-posts', (req, res) => {
+    //defines the required fields
+    const requiredFields = ['title', 'content', 'author', 'publishDate'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        //if the required fields are not in the body, send error message
+        if (!(field in req.body)) {
+            const message = `I'm sorry but you are still missing \`${field}\` in your post.`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    //if the required fields are there, we use BlogPosts from models to
+    //create a new blog post and then it will send a 201 code and a json blog post
+    //using the apiRepr we created in models.
+    BlogPosts
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author,
+            publishDate: req.body.publishDate
+        })
+        .then(
+            blogpost => res.status(201).json(blogpost.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                message: 'Internal server error'
+            });
+        });
+});
+
+app.put('/blog-posts/:id', (req, res) => {
+
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = (
+            `The id you have concatenated to the url (${req.params.id}) and the id you have included in your request ` +
+            `(${req.body.id}) must match`);
+        console.error(message);
+        res.status(400).json({
+            message: message
+        });
+    }
+
+    const toUpdate = {};
+    const updateableFields = ['title', 'content', 'author', 'publishDate'];
+
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+
+    BlogPosts
+        //what are the two lines of code below doing?
+        .findByIdAndUpdate(req.params.id, {
+            $set: toUpdate
+        })
+        .exec()
+        .then(blogpost => res.status(204).end())
+        .catch(err => res.status(500).json({
+            message: 'Internal server error'
+        }));
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts
+        .findByIdAndRemove(req.params.id)
+        .exec()
+        .then(() => res.status(204).end())
+        .catch(err => res.status(500).json({
+            message: 'Internal server error'
+        }));
+});
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function (req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
 
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
@@ -42,14 +172,21 @@ let server;
 // this function starts our server and returns a Promise.
 // In our test code, we need a way of asynchronously starting
 // our server, since we'll be dealing with promises there.
-function runServer() {
-    const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+    //const port = process.env.PORT || 8080;
     return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err)
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
         });
     });
 }
@@ -58,17 +195,18 @@ function runServer() {
 // `server.close` does not return a promise on its own, so we manually
 // create one.
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                // so we don't also call `resolve()`
-                return;
-            }
-            resolve();
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+            });
         });
     });
+
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
